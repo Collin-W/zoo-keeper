8bbbc88e6884cfb351d5fa9bb44a0c732cdaf293
Collin-W <whalencollin@gmail.com>
2021-10-28 10:14:05 -0500

    validates and adds post data

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 // any npm package needs 
 const express = require('express')
+const fs = require('fs');
+const path = require('path');
 
 const { animals } = require('./data/animals.json')
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3009;
 
 //starts express
 const app = express();
+
+
+//need to be set up if you wanna use POST
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -47,6 +56,18 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimals(body, animalsArray) {
+// our function's main code will go here!
+const animal = body;
+animalsArray.push(animal);
+fs.writeFileSync(
+  path.join(__dirname, './data/animals.json'),
+  JSON.stringify({ animals: animalsArray }, null, 2)
+);
+// return finished code to post route for response
+return animal;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -64,12 +85,44 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+app.post('/api/animals', (req, res) => {
+  console.log(req.body);
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if(!validateAnimal(req.body)) {
+    res.status(400).send('the animal not properly formatted');
+  } else {
+    // add animal to json file and animals array in this function
+  const animal = createNewAnimals(req.body, animals);
+
+  res.json(animal);
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`)
 })
 
+function validateAnimal(animal) {
+  //checks if key exists and if it is right data type
+  if(!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
 
+//createNewAnimals()
 
   
 
